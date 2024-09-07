8246b5ed5ab28ed23c53e5c432f0b5318e5d6b07
hoverboardhavoc <hoverboardhavoc.county449@passmail.net>
2024-09-07 11:13:36 +0200

    Test ITG3200 library

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,19 @@
 #include <Arduino.h>
-#include <I2CScanner.h>
-
-I2CScanner scanner;
-
-void debug(byte address)
-{
-  Serial2.print("Found at 0x");
-  Serial2.println(address, HEX);
-}
-
+#include <ITG3200.h>
+ITG3200 itg;
 void setup()
 {
   Serial2.begin(19200);
   while (!Serial2)
   {
-  };
-  Serial2.print("Hello world");
-  scanner.Init();
+  }
+  Serial2.println("Hello");
+  itg.initialize();
 }
 
 void loop()
 {
-  scanner.Execute(debug);
-  delay(5000);
+  Serial2.println(itg.x());
+  Serial2.println(itg.y());
+  Serial2.println(itg.z());
 }
